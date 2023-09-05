25ea9db2189f067f2334f8a716123dcde747ca0c
EshikaP1 <fairspringgal@gmail.com>
2023-09-05 12:51:31 -0700

    commit6

--- _posts/hackman.py
+++
@@ -1,7 +0,0 @@
-import random
-import requests
-#from is used to import modules or libraries from python
-#BeautifulSoup is a class which helps formating between HTML and XML documents and python 
-from bs4 import BeautifulSoup
-#
-
